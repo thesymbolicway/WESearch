b6b3323c13dcf9a28dcbebc7875b535c498c64fb
Daniel Arisa <daniel.arisa@yahoo.com>
2022-10-06 06:37:10 -0500

    Added the star count, not sure how to display both

--- src/pages/SingleReview.js
+++ src/pages/SingleReview.js
@@ -1,8 +1,30 @@
- export default function SingleReview({review}){
+ import { useState } from 'react'
+ 
+ function SingleReview({review}){
+ const [rating, setRating] = useState(review.star_rating); 
+
+ if (rating === 1){
+    return "⭐"
+ }
+ else if (rating === 2){
+    return "⭐⭐"
+ }
+else if (rating === 3){
+   return "⭐⭐⭐"
+}
+else if (rating === 4){
+   return "⭐⭐⭐⭐"
+}
+else if (rating === 5){
+   return "⭐⭐⭐⭐⭐"
+}
+    
     return (
         <>
             <p>"{review.review}" - <span className="span-tag">{review.user.name}</span> </p>
-            <p>Stars: <span className="span-tag">{review.star_rating}</span></p>
+            
+            <p>Stars: <span className="span-tag">{setRating}</span></p>
         </>
     )
 }
+export default SingleReview
